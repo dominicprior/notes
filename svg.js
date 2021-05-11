1fd347633d2844473ba4b37deb13802c2a8511ef
Dominic Prior <dominicprior42@gmail.com>
2021-05-11 14:34:36 +0100

    Update svg.js

--- svg.js
+++ svg.js
@@ -1,65 +1,96 @@
 'use strict'
+
 function drawTree(numLevels, matrices, group,
                   width, height, colour) {
-  if (numLevels == 0) {
-    return
-  }
   group.rect(width, height).fill(colour).cx(0).opacity(1)
-          .radius(width/3)
-  for (let matrix of matrices) {
-    let subGroup = group.group().transform(matrix)
-    drawTree(numLevels - 1, matrices, subGroup,
-             width, height, colour)
+  if (numLevels > 1) {
+    for (let matrix of matrices) {
+      let subGroup = group.group().transform(matrix)
+      drawTree(numLevels - 1, matrices, subGroup,
+               width, height, colour)
+    }
+  }
+}
+
+function updateTransforms(matrices, group) {
+  for (let child of group.children()) {
+    if (child.node.nodeName === 'g') {
+      //child.transform(matrix)
+      console.log(child.node)
+      updateTransforms(matrices, child)
+    }
   }
 }
 
 
 //window.addEventListener('load', (event) => {
-let windowWidth  = window.innerWidth
-let windowHeight = window.innerHeight
-let draw = SVG().addTo('body').size(windowWidth, windowHeight)
-let group = draw.group()
-               .transform({tx: windowWidth / 2,
-                           ty: 4 * windowHeight / 5,
-                           flip: 'y'
-               })
+
+let w  = window.innerWidth
+let h = window.innerHeight
+let draw = SVG().addTo('body').size(w, h)
+let mainGroup = draw.group()
+               .transform({tx: w / 2, ty: 4 * h / 5, flip: 'y'})
+
+// Q: Where will we flip the circle coords?
+// The events are on the "draw" and therefore need flipping
+// into the "mainGroup" coordinate system.
+// Maybe we should flip as soon as we receive the event.
+// Then all the logic will be in the mainGroup coordinate system.
+// Also the blob array can be in the mainGroup coordinate system.
+
+let blobs = [ [ [0, 300], [150, 580] ],
+              [ [0, 420], [-150, 640] ] ]
+
+let matrices = []
+
+for (let br of blobs) {  // e.g. [ [0, 300], [150, 580] ]
+  for (let end of br) {
+    mainGroup.circle(50).center(end[0], end[1]).fill('purple')
+  }
+  let a = (br[1][1] - br[0][1]) / (h / 3)
+  let b = (br[1][0] - br[0][0]) / (h / 3)
+  let matrix = new SVG.Matrix(a, -b, b, a, br[0][0], br[0][1])
+  matrices.push(matrix)
+}
 /*
-group.rect(windowHeight / 30, windowHeight / 3)
-               .fill('#f0f').cx(0)
-               .opacity(0.5)
-*/
 let uv = [0, 300]
 let duv = [150, 280]
-let x = duv[0] / (windowHeight / 3)
-let y = duv[1] / (windowHeight / 3)
+let x = duv[0] / (h / 3)
+let y = duv[1] / (h / 3)
 
-group.circle(20).center(uv[0], uv[1])
-group.circle(20).center(uv[0] + duv[0],
+mainGroup.circle(20).center(uv[0], uv[1])
+mainGroup.circle(20).center(uv[0] + duv[0],
                         uv[1] + duv[1])
 
 let matrix1 = new SVG.Matrix(y, -x, x, y, uv[0], uv[1])
 
 uv = [0, 420]
 duv = [-150, 220]
-x = duv[0] / (windowHeight / 3)
-y = duv[1] / (windowHeight / 3)
+x = duv[0] / (h / 3)
+y = duv[1] / (h / 3)
 
-group.circle(20).center(uv[0], uv[1])
-group.circle(20).center(uv[0] + duv[0],
+mainGroup.circle(20).center(uv[0], uv[1])
+mainGroup.circle(20).center(uv[0] + duv[0],
                         uv[1] + duv[1])
 
 let matrix2 = new SVG.Matrix(y, -x, x, y, uv[0], uv[1])
-
-drawTree(10, [matrix1, matrix2], group,
-         windowHeight / 30, windowHeight / 3, 'brown')
-/*
-let subGroup = group.group()
-        .transform({//translate: uv,
-                    //rotate: 30, scale: 0.5})
-        a: y, b: -x, c: x, d: y,
-        e: uv[0], f: uv[1]})
-let t = subGroup.transform()
-subGroup.rect(windowHeight / 30, windowHeight / 3)
-               .fill('#af0').cx(0)
-               .opacity(0.5)
 */
+drawTree(2, matrices, mainGroup,
+         h / 30, h / 3, 'brown')
+
+function coords(event) {
+  return [event.x - w / 2, 4 * h / 5 - event.y]
+}
+
+// note what blob we are moving and its offset from the mouse
+draw.mousedown((event) => {
+  let [x, y] = coords(event)
+  mainGroup.circle(20).center(x, y)
+  updateTransforms('not a matrix', mainGroup)
+})
+
+
+// Update the blob and a matrix and draw the whole tree again
+// by clearing stuff and calling drawTree again.
+// Or walk the tree poking the transform values.
+//draw.mousedown((event) => {
